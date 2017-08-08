b5f5a7dec9715134ecbeda717dae1ced0d717a9c
Joshua Wise <joshuathomaswise@gmail.com>
2017-08-07 23:46:23 -0400

    added Stream.from() and Promise#stream()

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 'use strict';
+const Promise = require('honest-promise');
+const HonestStream = require('./lib/honest-stream');
 
-module.exports = require('./lib/honest-stream');
+Promise.prototype.stream = function stream() {
+	return this.then(HonestStream.from);
+};
+
+module.exports = HonestStream;
+module.exports.Promise = Promise;
