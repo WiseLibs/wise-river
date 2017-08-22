716575c9cfa627d3a4a31f7f98f9b2ae9b540f95
Joshua Wise <joshuathomaswise@gmail.com>
2017-08-22 13:20:41 -0400

    added more tests

--- test/20.static-factory-functions.js
+++ test/20.static-factory-functions.js
@@ -1,6 +1,7 @@
 'use strict';
 const { expect } = require('chai');
 const River = require('../.');
+const makeIterable = require('../tools/make-iterable');
 
 describe('River.reject()', function () {
 	it('should return a rejected river', function () {
@@ -56,17 +57,113 @@ describe('River.empty()', function () {
 });
 
 describe('River.one()', function () {
-	
+	it('should return a river that emits only one item', function () {
+		const river = River.one(['foobar']);
+		expect(river).to.be.an.instanceof(River);
+		const data = [];
+		let tooLate = false;
+		river.pump(item => data.push(item));
+		setImmediate(() => tooLate = true);
+		return river.then(() => {
+			expect(data).to.deep.equal([['foobar']]);
+			expect(tooLate).to.equal(false);
+		});
+	});
+	it('should return a river that can be cancelled', function () {
+		const river = River.one(['foobar']);
+		river.pump(() => {})();
+		return expect(river).to.be.rejectedWith(River.Cancellation);
+	});
 });
 
 describe('River.from()', function () {
-	
+	it('should return a river that emits each item in the array', function () {
+		const river = River.from([['foo'], 'bar', 'baz']);
+		expect(river).to.be.an.instanceof(River);
+		const data = [];
+		let tooLate = false;
+		river.pump(item => data.push(item));
+		setImmediate(() => tooLate = true);
+		return river.then(() => {
+			expect(data).to.deep.equal([['foo'], 'bar', 'baz']);
+			expect(tooLate).to.equal(false);
+		});
+	});
+	it('should work with non-array iterable objects', function () {
+		const river = River.from(makeIterable([['foo'], 'bar', 'baz']));
+		expect(river).to.be.an.instanceof(River);
+		const data = [];
+		let tooLate = false;
+		river.pump(item => data.push(item));
+		setImmediate(() => tooLate = true);
+		return river.then(() => {
+			expect(data).to.deep.equal([['foo'], 'bar', 'baz']);
+			expect(tooLate).to.equal(false);
+		});
+	});
+	it('should return a rejected river if the argument is not an iterable object', function () {
+		const river = River.from(123);
+		expect(river).to.be.an.instanceof(River);
+		return expect(river).to.be.rejectedWith(TypeError);
+	});
+	it('should return a river that can be cancelled', function () {
+		const river = River.from([['foo'], 'bar', 'baz']);
+		river.pump(() => {})();
+		return expect(river).to.be.rejectedWith(River.Cancellation);
+	});
 });
 
 describe('River.every()', function () {
-	
+	it('should return a cancellable river that emits on an interval', function () {
+		const river = River.every(20);
+		expect(river).to.be.an.instanceof(River);
+		let count = 0;
+		let successes = 0;
+		let tooLate = false;
+		const cancel = river.pump((x) => { expect(x).to.equal(undefined); count += 1 });
+		const check = (expected) => () => { if (count === expected) successes += 1; }
+		setTimeout(check(0), 10);
+		setTimeout(check(1), 30);
+		setTimeout(check(2), 50);
+		setTimeout(() => { check(3)(); cancel(); }, 70);
+		setTimeout(() => tooLate = true, 80);
+		return new Promise(r => setTimeout(r, 75)).then(() => {
+			expect(count).to.equal(3);
+			expect(successes).to.equal(4);
+			return expect(river).to.be.rejectedWith(River.Cancellation);
+		}).then(() => {
+			expect(count).to.equal(3);
+			expect(successes).to.equal(4);
+			expect(tooLate).to.equal(false);
+			return new Promise(r => setTimeout(r, 50))
+		}).then(() => {
+			expect(count).to.equal(3);
+			expect(successes).to.equal(4);
+		});
+	});
+	it('should treat time values as 32-bit integers', function () {
+		const river = River.every('foobar');
+		expect(river).to.be.an.instanceof(River);
+		let count = 0;
+		const cancel = river.pump((x) => { expect(x).to.equal(undefined); count += 1 });
+		return new Promise(r => setTimeout(r, 20)).then(() => {
+			cancel();
+			expect(count).to.be.within(10, 30);
+		});
+	});
 });
 
 describe('River.combine()', function () {
-	
+	it('should return a river with the combined data of the given rivers', function () {
+		
+	});
+	it('should accept regular promises', function () {
+		
+	});
+	it('should not affect any arguments if iteration throws', function () {
+		
+	});
+	it('should cancel all given rivers when itself is cancelled', function () {
+		
+	});
 });
