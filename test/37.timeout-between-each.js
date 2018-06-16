5b707eb660ebc5fe4a8c40ac669e63b97b66971b
Joshua Wise <joshuathomaswise@gmail.com>
2018-06-16 01:35:32 -0400

    wrote tests for timeoutBetweenEach()

--- test/37.timeout-between-each.js
+++ test/37.timeout-between-each.js
@@ -2,5 +2,51 @@
 const { expect } = require('chai');
 const River = require('../.');
 
+const after = (ms, value) => new Promise((resolve) => {
+	setTimeout(() => resolve(value), ms);
+});
+
 describe('.timeoutBetweenEach()', function () {
+	it('should propagate rejections to the returned river', function () {
+		const err = new Error('foobar');
+		const river = River.one(new Promise((_, r) => setTimeout(() => r(err), 10)));
+		return expect(river.timeoutBetweenEach(30)).to.be.rejectedWith(err);
+	});
+	it('should propagate cancellations back to the source river', function () {
+		const river = River.every(10);
+		river.timeoutBetweenEach(30).pump(() => {})();
+		return expect(river).to.be.rejectedWith(River.Cancellation);
+	});
+	it('should not timeout if data is regularly received', function () {
+		const promise = River.from([after(10, 'a'), after(30, 'b'), after(50, 'c')])
+			.timeoutBetweenEach(30)
+			.all()
+			.then(arr => arr.join(''));
+		return expect(promise).to.become('abc');
+	});
+	it('should timeout if data is not received soon enough', function () {
+		const promise = River.from([after(10, 'a'), after(30, 'b'), after(70, 'c')])
+			.timeoutBetweenEach(30)
+			.all();
+		return expect(promise).to.be.rejectedWith(River.TimeoutError);
+	});
+	it('should accept a string reason', function () {
+		const promise = River.from([after(10, 'a'), after(30, 'b'), after(70, 'c')])
+			.timeoutBetweenEach(30, 'foobar')
+			.all();
+		return expect(promise).to.be.rejectedWith(/^foobar$/);
+	});
+	it('should accept an Error reason', function () {
+		const err = new TypeError('foobar');
+		const promise = River.from([after(10, 'a'), after(30, 'b'), after(70, 'c')])
+			.timeoutBetweenEach(30, err)
+			.all();
+		return expect(promise).to.be.rejectedWith(err);
+	});
+	it('should start the timer right away', function () {
+		const promise = River.from([after(40, 'a'), after(50, 'b'), after(60, 'c')])
+			.timeoutBetweenEach(30)
+			.all();
+		return expect(promise).to.be.rejectedWith(River.TimeoutError);
+	});
 });
--- test/38.while.js
+++ test/38.while.js
@@ -1,6 +1,28 @@
 'use strict';
 const { expect } = require('chai');
 const River = require('../.');
+const invalidArgs = require('../tools/invalid-args');
 
 describe('.while()', function () {
+	it('should return a rejected river if invalid arguments are given', function () {
+		const testRejected = (value) => {
+			expect(value).to.be.an.instanceof(River);
+			return expect(value).to.be.rejectedWith(TypeError);
+		};
+		return Promise.all(invalidArgs().map(args => testRejected(River.from(['a']).while(...args))));
+	});
+	it('should propagate rejections to the returned river', function () {
+		const err = new Error('foobar');
+		const river = River.one(new Promise((_, r) => setTimeout(() => r(err), 10)));
+		return expect(river.while(() => true)).to.be.rejectedWith(err);
+	});
+	it('should propagate cancellations back to the source river', function () {
+		const river = River.every(10);
+		river.while(() => true).pump(() => {})();
+		return expect(river).to.be.rejectedWith(River.Cancellation);
+	});
+	it('should invoke the callback to determine a stopping point');
+	it('should respect a given concurrency value');
+	it('should reject the stream if the handler throws');
+	it('should reject the stream if the handler returns a rejected promise');
 });
