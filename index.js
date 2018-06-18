1c9d8cef4c85f84e8ab5b1495e9613ef87e36290
Joshua Wise <joshuathomaswise@gmail.com>
2018-06-18 02:32:42 -0400

    wrote tests for WisePromise#stream()

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ const WiseRiver = require('./lib/wise-river');
 const notIterable = x => x == null || typeof x[Symbol.iterator] !== 'function';
 Object.defineProperty(Promise.prototype, 'stream', {
 	writable: true,
-	enumerable: false,
 	configurable: true,
 	value: function stream() {
 		return new WiseRiver((resolve, reject, write) => {
 			this.then((iterable) => {
-				if (notIterable(iterable)) throw new TypeError('Expected promise to be resolved with an iterable object');
-				for (const item of iterable) write(item);
-				resolve();
-			}).catch(reject);
+				try {
+					if (notIterable(iterable)) throw new TypeError('Expected promise to be resolved with an iterable object');
+					for (const item of iterable) write(item);
+					resolve();
+				} catch (reason) {
+					reject(reason);
+				}
+			}, reject);
 		});
 	}
 });
--- test/49.stream.js
+++ test/49.stream.js
@@ -1,16 +1,58 @@
 'use strict';
 const { expect } = require('chai');
+const makeIterable = require('../tools/make-iterable');
 const River = require('../.');
+const WisePromise = River.Promise;
 
 describe('.stream()', function () {
-	it('should just return the same river', function () {
-		let str = '';
-		const river = River.from(['a', 'b', 'c']);
-		const result = river.stream();
-		expect(river).to.equal(result);
-		river.pump(x => str += x);
-		return expect(river).to.become(undefined).then(() => {
-			expect(str).to.equal('abc');
+	describe('for WiseRiver', function () {
+		it('should just return the same river', function () {
+			let str = '';
+			const river = River.from(['a', 'b', 'c']);
+			const result = river.stream();
+			expect(river).to.equal(result);
+			river.pump(x => str += x);
+			return expect(river).to.become(undefined).then(() => {
+				expect(str).to.equal('abc');
+			});
+		});
+	});
+	describe('for WisePromise', function () {
+		it('should propagate rejections to the returned river', function () {
+			const err = new Error('foobar');
+			const promise = new WisePromise((_, r) => setTimeout(() => r(err), 10));
+			const river = promise.stream();
+			expect(river).to.be.an.instanceof(River);
+			return expect(river).to.be.rejectedWith(err);
+		});
+		it('should return a river that emits each item in the obtained array', function () {
+			const data = [];
+			const river = WisePromise.resolve([['foo'], 'bar', 'baz']).stream();
+			expect(river).to.be.an.instanceof(River);
+			river.pump(item => data.push(item));
+			return river.then(() => {
+				expect(data).to.deep.equal([['foo'], 'bar', 'baz']);
+			});
+		});
+		it('should work with promises of non-array iterable objects', function () {
+			const data = [];
+			const river = WisePromise.resolve(makeIterable([['foo'], 'bar', 'baz'])).stream();
+			expect(river).to.be.an.instanceof(River);
+			river.pump(item => data.push(item));
+			return river.then(() => {
+				expect(data).to.deep.equal([['foo'], 'bar', 'baz']);
+			});
+		});
+		it('should return a rejected river if an iterable is not obtained', function () {
+			const river = WisePromise.resolve(123).stream();
+			expect(river).to.be.an.instanceof(River);
+			return expect(river).to.be.rejectedWith(TypeError);
+		});
+		it('should return a rejected river if iteration throws an exception', function () {
+			const err = new Error('foobar');
+			const river = WisePromise.resolve({ [Symbol.iterator]: () => ({ next() { throw err; } }) }).stream();
+			expect(river).to.be.an.instanceof(River);
+			return expect(river).to.be.rejectedWith(err);
 		});
 	});
 });
