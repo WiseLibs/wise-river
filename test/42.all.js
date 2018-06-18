df423c7c42b379e4e9eb384fe722dae8d28bf6fd
Joshua Wise <joshuathomaswise@gmail.com>
2018-06-18 00:23:18 -0400

    added tests for .all()

--- test/42.all.js
+++ test/42.all.js
@@ -3,4 +3,15 @@ const { expect } = require('chai');
 const River = require('../.');
 
 describe('.all()', function () {
+	it('should propagate rejections to the returned promise', function () {
+		const err = new Error('foobar');
+		const river = River.one(new Promise((_, r) => setTimeout(() => r(err), 10)));
+		return expect(river.all()).to.be.rejectedWith(err);
+	});
+	it('should resolve to an array of all items provided', function () {
+		return expect(River.from(['a', Promise.resolve('b'), 'c']).all()).to.become(['a', 'c', 'b']);
+	});
+	it('should resolve to an empty array when no items are provided', function () {
+		return expect(River.from([]).all()).to.become([]);
+	});
 });
