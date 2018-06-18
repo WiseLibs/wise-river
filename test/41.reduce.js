4e6e51bdd120d13ac33dd4b00a2f48cdaf3e6f3b
Joshua Wise <joshuathomaswise@gmail.com>
2018-06-17 23:43:28 -0400

    added placeholder tests for .reduce()

--- test/41.reduce.js
+++ test/41.reduce.js
@@ -3,4 +3,15 @@ const { expect } = require('chai');
 const River = require('../.');
 
 describe('.reduce()', function () {
+	it('should return a rejected promise if invalid arguments are given', function () {});
+	it('should propagate rejections to the returned promise', function () {});
+	it('should apply the reducer to a river of items', function () {});
+	it('should accept an initial value', function () {});
+	it('should accept an initial value as a promise', function () {});
+	it('should not invoke the callback when only one item is provided', function () {});
+	it('should not invoke the callback when only an initial value is provided', function () {});
+	it('should return a NoDataError when no values are provided', function () {});
+	it('should use a concurrency of 1 when the callback returns a promise', function () {});
+	it('should reject the promise if the handler throws', function () {});
+	it('should reject the promise if the handler returns a rejected promise', function () {});
 });
